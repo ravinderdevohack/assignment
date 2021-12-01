3feea6172039d13f3a566987ade73990efbd43bf
ravinder <ravinder@devohack.com>
2021-12-01 11:07:31 +0530

    Added first.rb file

---
+++ first.rb
@@ -0,0 +1,48 @@
+# x = 'ravinder'
+# puts "Hi my name is "
+# puts "#{x}"
+
+
+# # ------------------------------------------------------------------------------------
+# # create hash in 2 ways
+
+var = {a: 1, b: 2}
+car = {"a"=>"ravi", "b"=>"inder"}
+
+puts var[:a]
+puts car["a"]
+
+# add a new key 
+var["a"]= 3    
+
+# # call value of key
+# puts var[:a]         
+
+
+# # print all key
+# puts var.keys
+
+# # print all keys values
+# puts var.values
+
+
+# var[:a]='5'
+# puts var[:a]
+
+
+
+# # print all keys values
+# puts var.values
+
+
+
+# var ={ "a": '1', "a"=>'1'}
+
+# puts var.values
+# puts var[:a]
+# puts var["a"]
+
+var ={}
+var[:a]='sachin'
+
+puts var.length
